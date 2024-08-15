e44823fc9f6f7b977a2af2cdfc4508ced9ac681f
Shehab ElDin Mohsen Hamed <shehabmohsenhamed@gmail.com>
2024-08-15 21:54:45 +0300

    init Day04

---
+++ Day04/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Socials from "./components/Socials";
+
+function App() {
+  return (
+    <div className="whole-page">
+      <Socials />
+    </div>
+  );
+}
+
+export default App;
