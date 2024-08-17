71fcacc83e490d22c398f00a537b44cc49588c78
Shehab ElDin Mohsen Hamed <shehabmohsenhamed@gmail.com>
2024-08-17 19:45:22 +0300

    Add Day05 tsx setup (Day05)

---
+++ Day05/src/components/Skillbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function Skillbar() {
+  return (
+    <div className="skill-bar">
+      <div className="bar">
+        <div className="percent">
+          <span>HTML</span>
+        </div>
+        <div className="progress-line html">
+          <span></span>
+        </div>
+      </div>
+      <div className="bar">
+        <div className="percent">
+          <span>CSS</span>
+        </div>
+        <div className="progress-line css">
+          <span></span>
+        </div>
+      </div>
+      <div className="bar">
+        <div className="percent">
+          <span>Javascript</span>
+        </div>
+        <div className="progress-line JS">
+          <span></span>
+        </div>
+      </div>
+      <div className="bar">
+        <div className="percent">
+          <span>React</span>
+        </div>
+        <div className="progress-line react">
+          <span></span>
+        </div>
+      </div>
+      <div className="bar">
+        <div className="percent">
+          <span>Vue</span>
+        </div>
+        <div className="progress-line vue">
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
