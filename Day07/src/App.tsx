c3851005b0cd6c10dd858458c6d6f3767f9e22c7
Shehab ElDin Mohsen Hamed <shehabmohsenhamed@gmail.com>
2024-08-20 11:03:38 +0300

    init Day07

---
+++ Day07/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import RPS from "./components/RPS";
+
+function App() {
+  return (
+    <div className="whole-page">
+      <RPS />
+    </div>
+  );
+}
+
+export default App;
