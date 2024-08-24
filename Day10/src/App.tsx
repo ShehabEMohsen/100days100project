1e5dc69a96de7893d54f035750b3842e1637bece
Shehab ElDin Mohsen Hamed <shehabmohsenhamed@gmail.com>
2024-08-24 20:54:31 +0300

    init day10

---
+++ Day10/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Questions from "./components/Questions";
+
+function App() {
+  return (
+    <div>
+      <Questions />
+    </div>
+  );
+}
+
+export default App;
