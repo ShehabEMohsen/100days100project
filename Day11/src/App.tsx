d7302589804482cf52c59932f6335d2bc5bcb5f5
Shehab ElDin Mohsen Hamed <shehabmohsenhamed@gmail.com>
2024-08-26 16:42:37 +0300

    init day11

---
+++ Day11/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import PriceCard from "./components/PriceCard";
+
+function App() {
+  return (
+    <div>
+      <PriceCard />
+    </div>
+  );
+}
+
+export default App;
